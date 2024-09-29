3336aada50f4939ab123bfcc3f63e6624da9afb8
Md. Ahnaf Muttaqi Chowdhury <ahnaf.muttaqi@welldev.io>
2024-09-29 13:35:17 +0600

    sidekiq_app_mail

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,33 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      SendWelcomeEmailJob.perform_later(@user)
+      redirect_to root_path, notice: 'User was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def delete
+  end
+
+  def update
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email)
+  end
+end
